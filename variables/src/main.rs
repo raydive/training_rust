386c40c5d039d939fadb1ecef98b0e743b319abd
raydive <raywassmas@gmail.com>
2022-05-03 18:45:20 +0900

    The Rust Programming Book Chapter 3.1 変数と可変性

---
+++ variables/src/main.rs
@@ -0,0 +1,7 @@
+// https://doc.rust-jp.rs/book-ja/ch03-01-variables-and-mutability.html
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {}", x);     // xの値は{}です
+    x = 6;
+    println!("The value of x is: {}", x);
+}
