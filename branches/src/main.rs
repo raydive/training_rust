b3cb25aa8919b80db7e8e3789b868300bfe48e65
raydive <raywassmas@gmail.com>
2022-05-06 00:27:01 +0900

    The Rust Programming Book Chapter 3
    Add control flow

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -9,20 +9,66 @@
 // }
 // https://doc.rust-jp.rs/book-ja/ch03-05-control-flow.html
 
-fn main() {
-    let number = 6;
+// fn main() {
+//     let number = 6;
 
-    if number % 4 == 0 {
-        // 数値は4で割り切れます
-        println!("number is divisible by 4");
-    } else if number % 3 == 0 {
-        // 数値は3で割り切れます
-        println!("number is divisible by 3");
-    } else if number % 2 == 0 {
-        // 数値は2で割り切れます
-        println!("number is divisible by 2");
-    } else {
-        // 数値は4、3、2で割り切れません
-        println!("number is not divisible by 4, 3, or 2");
+//     if number % 4 == 0 {
+//         // 数値は4で割り切れます
+//         println!("number is divisible by 4");
+//     } else if number % 3 == 0 {
+//         // 数値は3で割り切れます
+//         println!("number is divisible by 3");
+//     } else if number % 2 == 0 {
+//         // 数値は2で割り切れます
+//         println!("number is divisible by 2");
+//     } else {
+//         // 数値は4、3、2で割り切れません
+//         println!("number is not divisible by 4, 3, or 2");
+//     }
+// }
+
+// fn main() {
+//     let condition = true;
+//     let number = if condition {
+//         5
+//     } else {
+//         6
+//     };
+
+//     // numberの値は、{}です
+//     println!("The value of number is: {}", number);
+// }
+
+// fn main() {
+//     let mut number = 3;
+
+//     while number != 0 {
+//         println!("{}!", number);
+
+//         number = number - 1;
+//     }
+
+//     // 発射！
+//     println!("LIFTOFF!!!");
+// }
+
+
+// fn main() {
+//     let a = [10, 20, 30, 40, 50];
+//     let mut index = 0;
+
+//     while index < 5 {
+//         // 値は{}です
+//         println!("the value is: {}", a[index]);
+
+//         index = index + 1;
+//     }
+// }
+fn main() {
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a.iter() {
+        // 値は{}です
+        println!("the value is: {}", element);
     }
 }
