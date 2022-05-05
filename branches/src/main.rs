668c95337660d46c37e6911c8ce8a6816a7c75fd
raydive <raywassmas@gmail.com>
2022-05-05 14:43:41 +0900

    Check if else statements

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,10 +1,28 @@
-fn main() {
-    let number = 3;
+// fn main() {
+//     let number = 3;
 
-    if number < 5 {
-        println!("condition was true");       // 条件は真でした
+//     if number < 5 {
+//         println!("condition was true");       // 条件は真でした
+//     } else {
+//         println!("condition was false");      // 条件は偽でした
+//     }
+// }
+// https://doc.rust-jp.rs/book-ja/ch03-05-control-flow.html
+
+fn main() {
+    let number = 6;
+
+    if number % 4 == 0 {
+        // 数値は4で割り切れます
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        // 数値は3で割り切れます
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        // 数値は2で割り切れます
+        println!("number is divisible by 2");
     } else {
-        println!("condition was false");      // 条件は偽でした
+        // 数値は4、3、2で割り切れません
+        println!("number is not divisible by 4, 3, or 2");
     }
 }
-// https://doc.rust-jp.rs/book-ja/ch03-05-control-flow.html
