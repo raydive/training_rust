fa3acd7498061886d1038829c01d8a158409f8f5
raydive <raywassmas@gmail.com>
2022-05-09 22:51:28 +0900

    Chapter 6.1
    enum check

---
+++ chapter6/enum_test/src/main.rs
@@ -0,0 +1,22 @@
+#![allow(unused)]
+fn main() {
+    enum Message {
+        Quit,
+        Move { x: i32, y: i32 },
+        Write(String),
+        ChangeColor(i32, i32, i32),
+    }
+
+    impl Message {
+        fn call(&self) {
+            // method body would be defined here
+            // メソッド本体はここに定義される
+            println!(
+                "call!"
+            );
+        }
+    }
+
+    let m = Message::Write(String::from("hello"));
+    m.call();
+}
