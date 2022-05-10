392c8a98aa789f4d342cd01ef6f8d3bb7ae72ff4
raydive <raywassmas@gmail.com>
2022-05-10 21:34:30 +0900

    Chapter 7
    Add cannot build code project

---
+++ chapter7/cannot_compile/src/lib.rs
@@ -0,0 +1,16 @@
+// Compiler can not build this code
+mod front_of_house {
+    mod hosting {
+        fn add_to_waitlist() {}
+    }
+}
+
+pub fn eat_at_restaurant() {
+    // Absolute path
+    // 絶対パス
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    // 相対パス
+    front_of_house::hosting::add_to_waitlist();
+}
