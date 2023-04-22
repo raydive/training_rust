d558a96c991605bea99c3526993deaea23a3a05c
raydive <raywassmas@gmail.com>
2023-04-22 17:54:15 +0900

    12.3 run関数を追加して、エラー処理する

--- chapter12/minigrep/src/main.rs
+++ chapter12/minigrep/src/main.rs
@@ -2,6 +2,7 @@ use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 use std::process;
+use std::error::Error;
 
 struct Config {
     query: String,
@@ -32,11 +33,20 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let mut f = File::open(config.filename).expect("file not found");
 
     let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("Something went wrong reading the file");
+    f.read_to_string(&mut contents)?;
     
     println!("With text:\n{}", contents);
+
+    Ok(())
 }
