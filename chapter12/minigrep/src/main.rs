925e204bf68cdb4f04259c30e1a92dd2155de252
raydive <raywassmas@gmail.com>
2023-04-22 18:00:55 +0900

    12.3 lib.rsに移動するようリファクタリング

--- chapter12/minigrep/src/main.rs
+++ chapter12/minigrep/src/main.rs
@@ -1,26 +1,7 @@
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
 use std::process;
-use std::error::Error;
 
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-    
-        Ok(Config { query: query, filename: filename })
-    }
-}
+use minigrep::{Config, run};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -38,15 +19,4 @@ fn main() {
 
         process::exit(1);
     }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let mut f = File::open(config.filename).expect("file not found");
-
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)?;
-    
-    println!("With text:\n{}", contents);
-
-    Ok(())
 }
