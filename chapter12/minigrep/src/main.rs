1d76943e4b11a7aa4bc036863584b2c97a56fc57
raydive <raywassmas@gmail.com>
2023-04-22 17:33:54 +0900

    12.3途中まで

--- chapter12/minigrep/src/main.rs
+++ chapter12/minigrep/src/main.rs
@@ -2,16 +2,33 @@ use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+    
+        Config { query: query, filename: filename }
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args);
+    
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
-
-    let mut f = File::open(filename).expect("file not found");
+    let mut f = File::open(config.filename).expect("file not found");
 
     let mut contents = String::new();
     f.read_to_string(&mut contents)
