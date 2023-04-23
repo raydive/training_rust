5c4dc2a9019e82e2e1d18407565adbd2f964d573
raydive <raywassmas@gmail.com>
2023-04-23 14:27:23 +0900

    13.1 closure導入

--- chapter13/startup/src/main.rs
+++ chapter13/startup/src/main.rs
@@ -1,13 +1,6 @@
 use std::thread;
 use std::time::Duration;
 
-fn simulated_expensive_calculation(intensity: u32) -> u32 {
-    // ゆっくり計算します
-    println!("calculating slowly...");
-    thread::sleep(Duration::from_secs(2));
-    intensity
-}
-
 fn main() {
     let simulated_user_specified_value = 10;
     let simulated_random_number = 7;
@@ -18,19 +11,23 @@ fn main() {
     );
 }
 fn generate_workout(intensity: u32, random_number: u32) {
-    let expensive_result = simulated_expensive_calculation(intensity);
+    let expensive_closure = |num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    };
 
     if intensity < 25 {
         println!(
             // 今日は{}回腕立て伏せをしてください！
             "Today, do {} pushups!",
-            expensive_result
+            expensive_closure(intensity)
         );
 
         println!(
             // 次に、{}回腹筋をしてください！
             "Next, do {} situps!",
-            expensive_result
+            expensive_closure(intensity)
         );
     } else {
         if random_number == 3 {
@@ -40,7 +37,7 @@ fn generate_workout(intensity: u32, random_number: u32) {
             println!(
                 // 今日は、{}分間走ってください！
                 "Today, run for {} minutes!",
-                expensive_result
+                expensive_closure(intensity)
             );
         }
     }
