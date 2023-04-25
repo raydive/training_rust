c3f979bc885745720b502ac416dc16de5f1401f9
raydive <raywassmas@gmail.com>
2023-04-25 09:08:43 +0900

    13.2 他のイテレータを生成するメソッド

--- chapter13/iterator/src/lib.rs
+++ chapter13/iterator/src/lib.rs
@@ -21,5 +21,14 @@ mod tests {
         let total: i32 = v1_iter.sum();
     
         assert_eq!(total, 6);
-    }    
+    }
+
+    #[test]
+    fn vec_map() {
+        let v1: Vec<i32> = vec![1, 2, 3];
+
+        let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
+
+        assert_eq!(v2, vec![2, 3, 4]);
+    }
 }
