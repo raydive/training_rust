05a8f40d9135d0bd401c5a8a0c9b49f2fd40d83f
raydive <raywassmas@gmail.com>
2023-07-20 21:41:32 +0900

    15.4 Boxだとうまくいかないよという話

--- chapter15/cons/src/main.rs
+++ chapter15/cons/src/main.rs
@@ -7,4 +7,21 @@ enum List {
 
 fn main() {
     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+
+    let b = Cons(4, Box::new(list));
+    let c = Cons(5, Box::new(list));
 }
+
+/*
+error[E0382]: use of moved value: `list`
+  --> src/main.rs:12:30
+   |
+9  |     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+   |         ---- move occurs because `list` has type `List`, which does not implement the `Copy` trait
+10 |
+11 |     let b = Cons(4, Box::new(list));
+   |                              ---- value moved here
+12 |     let c = Cons(5, Box::new(list));
+   |                              ^^^^ value used here after move
+
+*/
