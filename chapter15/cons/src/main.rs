85602a4e64aa6c4b86829c1fa2d40e4b2445b84b
raydive <raywassmas@gmail.com>
2023-07-20 22:34:13 +0900

    15.4 Rcのカウントを見る

--- chapter15/cons/src/main.rs
+++ chapter15/cons/src/main.rs
@@ -1,27 +1,23 @@
 use crate::List::{Cons, Nil};
+use std::rc::Rc;
 
 enum List {
-    Cons(i32, Box<List>),
+    Cons(i32, Rc<List>),
     Nil,
 }
 
-fn main() {
-    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
-
-    let b = Cons(4, Box::new(list));
-    let c = Cons(5, Box::new(list));
-}
-
 /*
-error[E0382]: use of moved value: `list`
-  --> src/main.rs:12:30
-   |
-9  |     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
-   |         ---- move occurs because `list` has type `List`, which does not implement the `Copy` trait
-10 |
-11 |     let b = Cons(4, Box::new(list));
-   |                              ---- value moved here
-12 |     let c = Cons(5, Box::new(list));
-   |                              ^^^^ value used here after move
-
+count after creating list = 1
+count after creating list = 2
+count after creating list = 3
 */
+fn main() {
+    // これでコンパイル通る
+    let list = Rc::new(Cons(1, Rc::new(Cons(2, Rc::new(Cons(3, Rc::new(Nil)))))));
+
+    println!("count after creating list = {}", Rc::strong_count(&list));
+    let b = Cons(4, Rc::clone(&list));
+    println!("count after creating list = {}", Rc::strong_count(&list));
+    let c = Cons(5, Rc::clone(&list));
+    println!("count after creating list = {}", Rc::strong_count(&list));
+}
