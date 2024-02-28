e18d0c98e04d533e0e02c08fb519ae758b0e5aa4
raydive <raywassmas@gmail.com>
2024-02-28 16:03:52 +0900

    16.1 完

---
+++ chapter16/thread-test/src/main.rs
@@ -0,0 +1,22 @@
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            // やあ！立ち上げたスレッドから数字{}だよ！
+            println!("hi number {} from the spawned thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    handle.join().unwrap();
+
+    for i in 1..5 {
+        // メインスレッドから数字{}だよ！
+        println!("hi number {} from the main thread!", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    //handle.join().unwrap();
+}
