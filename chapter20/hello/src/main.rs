282ee84a41f56823c2573d12b2b9b677dce55f47
raydive <274594+raydive@users.noreply.github.com>
2026-01-23 20:55:37 +0900

    impl single threaded server

--- chapter20/hello/src/main.rs
+++ chapter20/hello/src/main.rs
@@ -19,13 +19,18 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
 
     stream.read(&mut buffer).unwrap();
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let get = b"GET / HTTP/1.1\r\n";
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
 
-    let mut file = File::open("hello.html").unwrap();
+    let mut file = File::open(filename).unwrap();
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
 
-    let responce = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
-    stream.write(responce.as_bytes()).unwrap();
+    let response = format!("{}{}", status_line, contents);
+    stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
